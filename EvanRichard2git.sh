03d54e02ea36d4949fa513cd1a1c7acae7a92028
EvanRichard2 <Evan.John.Richard@gmail.com>
2019-02-04 10:48:48 -0600

    idk

---
+++ EvanRichard2git.sh
@@ -0,0 +1,6 @@
+git config --list;
+git config --global user.username  (User);
+git config --global user.name  (User);
+git config --global user.user (User);
+git config --global user.email (Email);
+git config --list;
